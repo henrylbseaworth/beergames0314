170c1a04d8c8b0bae6c3516a4f5360d06128b90c
l3acon <l3acon@gmail.com>
2014-06-10 21:50:14 -0600

    added benchmark script

---
+++ dasboot/bench.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+then
+  echo "Usage: $0 <program> <key>"
+  exit 2
+fi
+if [[ ! -f programlist ]]; then
+  echo "Can't find programlist"
+  exit 2
+fi
+
+while read p; do
+
+  for fs in *.txt
+  do
+	  NAME=${fs%.txt}
+	  EXE=${1#*./}
+	  cp $fs $fs.tmp
+	  strace -c -o"BENCH_${NAME}_$EXE.d" -Ttt $1 $fs.tmp $2 
+	  rm $fs.tmp
+  done
+done <programslist
