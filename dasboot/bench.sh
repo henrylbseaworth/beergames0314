2839b1b62d64071be0056bef43f825da95b558d1
l3acon <l3acon@gmail.com>
2014-06-12 00:30:07 -0600

    updated benchmark script to actually work and added test files

--- dasboot/bench.sh
+++ dasboot/bench.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 if [ $# -eq 0 ]
 then
-  echo "Usage: $0 <program> <key>"
+  echo "Usage: $0 <key>"
   exit 2
 fi
 if [[ ! -f programlist ]]; then
@@ -9,14 +10,13 @@ if [[ ! -f programlist ]]; then
   exit 2
 fi
 
-while read p; do
-
-  for fs in *.txt
+while read PGRM; do
+  for TFILE in *.txt
   do
-	  NAME=${fs%.txt}
-	  EXE=${1#*./}
-	  cp $fs $fs.tmp
-	  strace -c -o"BENCH_${NAME}_$EXE.d" -Ttt $1 $fs.tmp $2
-	  rm $fs.tmp
+	  NAME=${TFILE%.txt}
+	  EXE=${PGRM#*./}
+	  cp $TFILE $TFILE.tmp
+	  strace -c -o"BENCH_${NAME}_$EXE.trace" -Ttt $PGRM $TFILE.tmp $1
+	  rm $TFILE.tmp
   done
 done <programlist
