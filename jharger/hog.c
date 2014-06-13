2c54b2b59ae5fab1568180813af432e7255ba53f
John Harger <john.harger@gmail.com>
2014-06-12 20:22:52 -0600

    Adding some nifty stuff

---
+++ jharger/hog.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    if(argc != 3) {
+        fprintf(stderr, "Usage: %s <filename> <key>\n", argv[0]);
+        return 1;
+    }
+    int fd = open(argv[1], O_RDWR);
+    if(fd < 0) {
+        fprintf(stderr, "Error opening file %s\n", argv[1]);
+        return 1;
+    }
+    const char *key = argv[2];
+    size_t keylen = strlen(key);
+
+    struct stat st;
+    fstat(fd, &st);
+
+    char *buf = (char*)malloc(st.st_size + 1);
+
+    read(fd, buf, st.st_size);
+
+    lseek(fd, 0, SEEK_SET);
+
+    char *ptr = buf, *end = (buf + st.st_size) - keylen;
+    for(;ptr < end; ptr ++) {
+        if(!bcmp(ptr, key, keylen)) {
+            *ptr = '\0';
+            ptr += keylen - 1;
+        }
+    }
+
+    const size_t outbuf_size = 131072;
+    char *outbuf = (char*)malloc(outbuf_size + 256);
+
+    ptr = buf;
+    int count = 0;
+    int out_offset = 0;
+    for(;ptr < end; ptr ++) {
+        if(*ptr == '\0') {
+            count ++;
+            out_offset += snprintf(outbuf + out_offset, 256, "%s(%d)", key, count);
+            ptr += keylen - 1;
+        } else {
+            *(outbuf + out_offset) = *ptr;
+            out_offset ++;
+        }
+
+        if(out_offset >= outbuf_size) {
+            write(fd, outbuf, out_offset);
+            out_offset = 0;
+        }
+    }
+    if(out_offset > 0) {
+        write(fd, outbuf, out_offset);
+    }
+
+    printf("Count: %d\n", count);
+
+    close(fd);
+
+    free(buf);
+    free(outbuf);
+
+    return 0;
+}
---
+++ jharger/my_bench.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#	This script runs every program in
+#	the file "programlist" using 
+#	every text file in the directory
+#	and a key provided with 
+#	command line arguments.
+#	There may be some issues with 
+#	escape characters and names.
+
+
+if [ $# -eq 0 ]
+then
+  echo "Usage: $0 <key>"
+  exit 2
+fi
+if [[ ! -f programlist ]]; then
+  echo "Can't find programlist"
+  exit 2
+fi
+
+PASSES=150
+
+while read PGRM; do
+  for TFILE in *.txt
+  do
+    TOTAL=0.0
+	for i in $(seq 1 $PASSES)
+      do
+	    NAME=${TFILE%.txt}
+	    EXE=${PGRM#*./}
+	    cp $TFILE $TFILE.tmp
+	    #strace -c -o"BENCH_${NAME}_$EXE.trace" -Ttt $PGRM $TFILE.tmp $1
+	    ELAPSED=`strace -c -Ttt $PGRM $TFILE.tmp $1 2>&1 >/dev/null | tail -n1 | awk '{ print $2 }' RS='\t'`
+        echo ELAPSED: $ELAPSED
+	    rm $TFILE.tmp
+        TOTAL=$(echo $TOTAL + $ELAPSED | bc)
+      done
+      echo TOTAL: $TOTAL AVG: $(echo "($TOTAL / $PASSES) * 1000000" | bc -l | sed s/[.].*//) us for $PGRM
+  done
+done <programlist
